507395d89728cb56523df9e5da5a470cc01e2bf8
zjgwh <76939221@qq.com>
2017-09-25 15:27:57 +0800

    1

---
+++ 代码/8.1/zqxt_tmp1/learn/views.py
@@ -0,0 +1,8 @@
+﻿# coding: utf-8
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):        
+	string = u"我在自强学堂学习Django，用它来建网站"
+	return render(request,'home.html',{'string':string})
