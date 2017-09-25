55e39e355e0e8574d6c170d492fe2b21c7aeef7f
zjgwh <76939221@qq.com>
2017-09-25 18:22:43 +0800

    1

---
+++ 代码/8.2/zqxt_tmp1/learn/views.py
@@ -0,0 +1,8 @@
+﻿# coding: utf-8
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+        TutorialList = ['HTML', 'CSS','JQuery','Python','Django']
+        return render(request,'home.html',{'TutorialList':TutorialList})
---
+++ 代码/8.3/zqxt_tmp1/learn/views.py
@@ -0,0 +1,10 @@
+﻿# coding: utf-8
+#显示字典中内容
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+        info_dict = {'site': u'自强学堂', 'content': u'各种IT技术教程'}
+        return render(request,'home.html',{'info_dict':info_dict})
