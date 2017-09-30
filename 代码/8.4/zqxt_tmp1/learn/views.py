6b48e7a60ec1c5becb4be7c3c725ef56acdde9b1
zjgwh <76939221@qq.com>
2017-09-30 08:15:50 +0800

    1

---
+++ 代码/8.4/zqxt_tmp1/learn/views.py
@@ -0,0 +1,10 @@
+﻿# coding: utf-8
+#实例四，在模板进行 条件判断和 for 循环的详细操作
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+        List = map(str,range(100))   # yi ge chang du wei 100 de List
+        return render(request, 'home.html',{'List':List})
